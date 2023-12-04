1852d34b31b550e4a32612de2efa7629375ebad6
Potykun <105052944+Potykun@users.noreply.github.com>
2023-12-04 13:37:51 +0100

    created testimonials

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import Qualification from "./components/Qualification/Qualification";
 import Services from "./components/Services/Services";
 import Skills from "./components/Skills/Skills";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 				<Skills></Skills>
 				<Services></Services>
 				<Qualification></Qualification>
+				<Testimonials></Testimonials>
 			</main>
 		</>
 	);
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./testimonials.css";
+import { Data } from "./Data";
+
+import { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper/modules";
+
+function Testimonials() {
+	return (
+		<div className="testimonial conteiner section">
+			<h2 className="section__title">My clients say</h2>
+			<span className="section__subtitle">Testimonials</span>
+			<Swiper
+				className="testimonial__conteiner"
+				loop={true}
+				spaceBetween={24}
+				grabCursor={true}
+				pagination={{
+					clickable: true,
+				}}
+				breakpoints={{
+					576: {
+						slidesPerView: 2,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 48,
+					},
+				}}
+				modules={[Pagination]}
+			>
+				{Data.map(({ id, image, title, description }) => {
+					return (
+						<SwiperSlide
+							className="testimonial__card"
+							key={id}
+						>
+							<img
+								src={image}
+								alt={`image${id}`}
+								className="testimonial__img"
+							/>
+							<h3 className="testimonial__name">{title}</h3>
+							<p className="testimonial__description">{description}</p>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</div>
+	);
+}
+
+export default Testimonials;
